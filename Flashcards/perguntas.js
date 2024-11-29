f90c8d04bd84970caddecf0c70e2301a11ef0633
VictorBarbosaDC <victorbarbosadc@gmail.com>
2024-11-28 21:59:53 -0300

    Add files via upload

---
+++ Flashcards/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'ANATOMIA',
+    'Quantos ossos temos no nosso corpo?',
+    'O corpo humano tem 206 ossos'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Em que região se localiza o estado de Minas Gerais?',
+    'Minas Gerais se encontra na região sudeste'
+)
+
+criaCartao(
+    'LITERATURA',
+    'Quem escreveu Os Lusíadas?',
+    'Luiz Vaz de Camões'
+)
+
+criaCartao(
+    'ASTRONOMIA',
+    'Qual o satélite natural da Terra?',
+    'O satélite natural da Terra é a Lua'
+)
+
+criaCartao(
+    'FILOSOFIA',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Renè Descartes'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango??',
+    'João Goulart'
+)
+
+criaCartao(
+    'ARTES',
+    'Quem pintou "Guernica"?',
+    'Pablo Picasso'
+)
+
+criaCartao(
+    'LINGUA PORTUGUESA',
+    'A palavra "alface" pertence a qual classe gramatical?',
+    'A palavra alface é um substantivo'
+)
